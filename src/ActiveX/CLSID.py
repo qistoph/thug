0ea8f88d64d9ece73fe3037cc042004062e3abdd
Chris van Marle <qistoph@gmail.com>
2016-06-02 11:36:22 +0200

    New features to support new sample

--- src/ActiveX/CLSID.py
+++ src/ActiveX/CLSID.py
@@ -139,7 +139,6 @@ CLSID = [
                             'type'          : 1,
                             'Type'          : 1,
                             'Mode'          : 3,
-                            'Position'      : 0,
                             'position'      : 0,
                             '_files'        : dict(),
                             '_current'      : None},
@@ -147,7 +146,6 @@ CLSID = [
             'methods'   : {
                             'Open'          : AdodbStream.open,
                             'Write'         : AdodbStream.Write,
-                            'SaveToFile'    : AdodbStream.SaveToFile,
                             'SaveTofile'    : AdodbStream.SaveToFile,
                             'LoadFromFile'  : AdodbStream.LoadFromFile,
                             'ReadText'      : AdodbStream.ReadText,
@@ -650,7 +648,7 @@ CLSID = [
         # MicrosoftXMLHTTP
         {
             'id'        : (),
-            'name'      : ( 'msxml2.xmlhttp', 'microsoft.xmlhttp', 'msxml2.xmlhttp.6.0'),
+            'name'      : ( 'msxml2.xmlhttp', 'microsoft.xmlhttp', 'msxml2.xmlhttp.6.0', 'winhttp.winhttprequest.5.1'),
             'attrs'     : {
                             'bstrMethod'            : '',
                             'bstrUrl'               : '',
